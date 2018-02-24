70ccc426192e2d322c16018a13db59c6d35fc51e
Alexandre Angelim <angelim@angelim.com.br>
2018-02-24 21:30:26 +0000

    more docs

--- gdpr/base_scrubber.rb
+++ gdpr/base_scrubber.rb
@@ -2,12 +2,19 @@ module Gdpr
   class BaseScrubber
     attr_reader :user_id
 
+    ##
+    # Generates the hash that will be used to update the record
+    # @param record [Model] the record, so we can use conditionals
+    #                       or any other required calculations.
+    #
+    # @return [Hash] update hash
+    # 
     def scrub_hash(record)
       raise NotImplementedError
     end
 
     ##
-    # @return [Array<Object>] list of records to scrub
+    # @return [Array<Model>] list of records to scrub
     #
     def gdpr_records
       raise NotImplementedError
@@ -24,6 +31,7 @@ module Gdpr
     ##
     # This is just an entry point forwarding all calls to the scrubbing method.
     # Allows calling `ChildScrubber.perform(arg1, arg2)`
+    #        Same as `ChildScrubber.new(arg1, arg2).scrub_records`
     #
     def self.perform(*args)
       new(*args).scrub_records
@@ -32,7 +40,7 @@ module Gdpr
     ##
     # Updates the record using the calculated
     # set of changed attributes
-    # @param record [#update_attributes] model to update
+    # @param record [Model] model to update
     #
     def scrub(record)
       hash = scrub_hash(record)
